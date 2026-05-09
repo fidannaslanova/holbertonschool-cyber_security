412b2defd54dae70d5dec09367bad67d0384d026
Fidan Aslanova <fidanaslanova44@gmail.com>
2026-05-09 13:32:06 -0400

    Fix 6-smtp.sh

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-result=$(grep -i "starttls\|tls" /etc/postfix/main.cf 2>/dev/null)
-if [ -z "$result" ]
-then
-    echo "STARTTLS not configured"
-else
-    echo "$result"
-fi
+grep -i "starttls\|tls" /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configured"
