66628cfa7b22ae3858f36042c0d89db4e3252d5d
Fidan Aslanova <fidanaslanova44@gmail.com>
2026-06-06 07:26:31 -0400

    Fix 6-check_files.sh

--- linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
+++ linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo find "$1" -type f -mtime -1 -perm /6000 -exec ls -la {} \; 2>/dev/null
+sudo find "$1" -type f -mtime -1 \( -perm -4000 -o -perm -2000 \) -exec ls -la {} \; 2>/dev/null
